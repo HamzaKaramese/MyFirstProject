f0342ad15738520d670a80354069eca1ea9a4bdb
Hamza Karamese <hamzakaramese@outlook.com>
2024-02-21 18:16:51 +0300

    SoundRecorder completed

---
+++ main.py
@@ -0,0 +1,17 @@
+import sounddevice
+
+from scipy.io.wavfile import write
+
+fs = 44100
+
+second = int(input("Enter time duration in seconds: "))
+
+print("Recordin....\n")
+
+record_voice = sounddevice.rec( int ( second * fs),samplerate= fs,channels= 2)
+
+sounddevice.wait()
+
+write("out.wav",fs,record_voice)
+
+print("Finished....\nPlease check your output file")
